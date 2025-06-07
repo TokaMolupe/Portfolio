ae8d039c4779213854d24ee5234fabbb7e0f9e17
TokaMolupe <molupetoka2006@gmail.com>
2025-06-07 21:31:14 +0200

    Optimize mobile responsiveness across devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <div>
             <Socials />
-            <div className="flex justify-between items-center gap-5 text-white p-6 mx-auto w-1/3 rounded-lg shadow-md shadow-lime-500 bg-transparent mt-5">
+            <div className="flex justify-between items-center gap-5 text-white p-6 mx-auto sm: w-full md:w-1/2 lg:w-1/3 rounded-lg shadow-md shadow-lime-500 bg-transparent mt-5">
                 <img src={tmicon} alt="Toka Molupe Icon" className="app-logo" />
                 <ul className="flex gap-6">
                     <li className="hover:text-lime-500 hover:text-lg">
